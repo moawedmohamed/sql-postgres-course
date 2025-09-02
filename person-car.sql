f598bb172e1b8690ab423c655660e4eb40055713
moawedmohamed <moawedmohamed144@gmail.com>
2025-09-03 01:06:18 +0300

    Update person-car.sql

--- person-car.sql
+++ person-car.sql
@@ -1,12 +1,4 @@
-create table person (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	gender VARCHAR(7) NOT NULL,
-	email VARCHAR(100),
-	date_of_birth DATE NOT NULL,
-	country_of_birth VARCHAR(50) NOT NULL
-);
+
 
 create table car (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -15,6 +7,18 @@ create table car (
 	price NUMERIC(19, 2) NOT NULL
 );
 
+
+create table person (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	gender VARCHAR(7) NOT NULL,
+	email VARCHAR(100),
+	date_of_birth DATE NOT NULL,
+	country_of_birth VARCHAR(50) NOT NULL,
+	car_id BIGINT references  car (id),
+	UNIQUE(car_id),
+);
 insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth) values ('Fernanda', 'Beardon', 'Female', 'fernandab@is.gd', '1953-10-28', 'Comoros');
 insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth) values ('Omar', 'Colmore', 'Male', null, '1921-04-03', 'Finland');
 insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth) values ('John', 'Matuschek', 'Male', 'john@feedburner.com', '1965-02-28', 'England');
